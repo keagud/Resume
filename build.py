0710eb850964b21c2b3e76e13468f36a06e96043
keagud <keagud@protonmail.com>
2023-03-01 14:12:28 -0800

    cleanup

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ if len(sys.argv) > 1:
 
 target_noext, _ = os.path.splitext(target_filename)
 target_path = os.path.join(os.getcwd(), target_filename)
-build_dir = os.path.join(os.getcwd(), f"build_{target_noext}")
+build_dir = os.path.join(os.getcwd(), f"build")
 os.makedirs(build_dir, exist_ok=True)
 
 shutil.copy(target_path, os.path.join(build_dir, f"{target_noext}.tex"))
