68c6120a4db748f7b43e4a19b9f7a1c334ca1c0a
keagud <keagud@protonmail.com>
2024-01-18 19:07:09 -0800

    add js script

--- html/convert.js
+++ html/convert.js
@@ -1,17 +1,61 @@
-const puppeteer = require("puppeteer");
+"use strict";
 
-const fs = require("fs");
-const path = require("path");
+const puppeteer = require('puppeteer');
+const Handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+const process = require('process')
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+/** @param {string} inputFile
+ * @param {string} styleFile
+ * @returns {string}
+ */
+function populateTemplateStyles(inputFile, styleFile) {
+  const styles = fs.readFileSync(styleFile, 'utf8');
+  const templateHtml = fs.readFileSync(inputFile, "utf8");
+  const template = Handlebars.compile(templateHtml);
+  const html = template({ styles: styles });
+  return html;
+}
+/** @param {string} inputFile
+ * @param {string} inputStylesFile
+ * @returns {string}
+ */
+function renderPdf(inputFile, inputStylesFile) {
+  (async () => {
+    const html = populateTemplateStyles(inputFile, inputStylesFile);
 
-  // Get the content of the file
-  const contentHtml = fs.readFileSync("./output_pdf.html", "utf8");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    await page.pdf({ path: "./output/output.pdf", format: "A4" });
+    await browser.close();
+  })();
 
-  await page.setContent(contentHtml);
-  await page.pdf({ path: "output.pdf", format: "A4" });
+}
 
-  await browser.close();
-})();
+function main() {
+  const pdfStyles = "./css/pdf.css";
+  const webStyles = "./css/web.css";
+
+  const baseFile = "./base.html"
+
+  const desiredFormat = process.argv[2];
+  console.log(desiredFormat);
+
+  switch (desiredFormat?.toLowerCase()) {
+    case "pdf":
+      renderPdf(baseFile, pdfStyles);
+      break;
+
+    case "web":
+      const webHtml = populateTemplateStyles(baseFile, webStyles);
+      fs.writeFileSync("./output/output.html", webHtml);
+      break;
+
+    default:
+      throw new Error(`Not a valid format ${desiredFormat}`);
+  }
+}
+
+main();
